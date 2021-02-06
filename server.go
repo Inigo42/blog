a6a251795bf6f267b472a63e8383e7eca136bf25
Jeremi F <jjfd428@gmail.com>
2021-02-05 21:10:38 -0500

    I got it so that the index.html displays actual article title. Problem with the hrefs being key-1

--- server.go
+++ server.go
@@ -14,8 +14,9 @@ import (
 
 //----------------database and whatnot------------------
 type Payload struct {
-	Stuff [3]string
-	Link  string
+	Stuff        []string
+	Link         string
+	ArticleLinks []string
 }
 
 var err error
@@ -50,21 +51,43 @@ func fetchArticle(db *leveldb.DB, key string, article *Article) {
 var templates *template.Template
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	words := [3]string{"first title", "second title", "third title"}
+	//words := []string{"first title", "second title", "third title"}
 	link2xkcd := "https://imgs.xkcd.com/comics/hug_count.png"
-	payload := Payload{Stuff: words, Link: link2xkcd}
+	db, err := leveldb.OpenFile("farticles", nil)
+	if err != nil {
+		panic("fuck")
+	}
+	defer db.Close()
+	var keys []string
+	iter := db.NewIterator(nil, nil)
+	for iter.Next() {
+		// Remember that the contents of the returned slice should not be modified, and
+		// only valid until the next call to Next.
+		keys = append(keys, string(iter.Key()))
+	}
+	var titles []string
+	var article Article
+	for _, key := range keys {
+		fetchArticle(db, key, &article)
+		title := article.Title
+		titles = append(titles, title)
+	}
+	payload := Payload{Stuff: titles, Link: link2xkcd, ArticleLinks: keys}
 	templates.ExecuteTemplate(w, "index.html", payload)
 }
 
+/*
 func testingHandler(w http.ResponseWriter, r *http.Request) {
-	stuff := [3]string{"blah1", "blah2", "blah3"}
+	stuff := []string{"blah1", "blah2", "blah3"}
 	link2xkcd := "https://imgs.xkcd.com/comics/hug_count.png"
 
 	payload := Payload{Stuff: stuff, Link: link2xkcd}
 	templates.ExecuteTemplate(w, "testing.html", payload)
 }
-
+*/
 func basicArticleHandler(w http.ResponseWriter, r *http.Request) {
+	k := r.URL.Path[1:]
+
 	db, err := leveldb.OpenFile("farticles", nil)
 	if err != nil {
 		panic("fuck")
@@ -72,16 +95,33 @@ func basicArticleHandler(w http.ResponseWriter, r *http.Request) {
 	defer db.Close()
 
 	var article Article
-	fetchArticle(db, "2", &article)
+	fetchArticle(db, k, &article)
 	templates.ExecuteTemplate(w, "article.html", article)
 }
 
 func main() {
+	/*
+		db, err := leveldb.OpenFile("farticles", nil)
+		if err != nil {
+			panic("fuck")
+		}
+		defer db.Close()
+		var keys []string
+		iter := db.NewIterator(nil, nil)
+		for iter.Next() {
+			// Remember that the contents of the returned slice should not be modified, and
+			// only valid until the next call to Next.
+			keys = append(keys, string(iter.Key()))
+		}
+		for _, key := range keys {
+			http.HandleFunc(key, basicArticleHandler)
+		}
+	*/
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 	fmt.Println("THIS WILL BE AN AWESOME BLOG!!!")
 
 	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/testing", testingHandler)
+	//http.HandleFunc("/testing", testingHandler)
 	http.HandleFunc("/test_article", basicArticleHandler)
 	http.ListenAndServe(":8080", nil)
 }
